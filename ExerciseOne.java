3d570a00f53d5027b7d33693df56e73138bcb0fc
DivideHanBy3 <dividehanby3@gmail.com>
2019-07-09 10:02:18 -0400

    AI2 Day 1 Exercises

---
+++ ExerciseOne.java
@@ -0,0 +1,7 @@
+public class ExerciseOne{ //Start of program
+	public static void main(String[] args) { //start main method
+		System.out.print("David ");
+		System.out.print("Han ");
+		System.out.println("is a rising Sophomore");
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{ //Start of program
+	public static void main(String[] args) { //start main method 
+		System.out.println("Hello, world!");
+	}
+}
